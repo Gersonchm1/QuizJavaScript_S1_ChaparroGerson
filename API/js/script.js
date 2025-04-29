51570baa242e8a5c11848cd008c0b0a898b803ed
Gersonchm1 <gersonchaparromartinez@gmail.com>
2025-04-28 21:37:10 -0500

    feat: :fire: new changes

--- API/js/script.js
+++ API/js/script.js
@@ -1,7 +1,7 @@
 
 function fetch(){
     let xhr = new XMLHttpRequest();
-    let link =`http://api.weatherapi.com/v1/current.json?key=587e3ce13a5944f8a94115014252804&q=Kharkiv&lang=es`
+    let link =`http://api.weatherapi.com/v1/forecast.json?key=587e3ce13a5944f8a94115014252804&q=Bucaramanga&lang=es`
     xhr.open("GET",link, true);
     xhr.onreadystatechange = function(){
         if (xhr.readyState=== 4 && xhr.status === 200){
@@ -12,7 +12,43 @@ function fetch(){
             country_name.innerHTML =`
             <h1 class="country-name">${data["location"]["name"]}, ${data["location"]["country"]}
             </h1>
-            <h1 class="temperature">${data["current"]["temp_c"]}</h1>
+            <h1 class="temperature">${data["current"]["temp_c"]}°C</h1>
+            <h1 class="localtime">${data["location"]["localtime"]}</h1>
+            <h1 class="condition">
+            <p class="hour">${data.forecast.forecastday[0].hour[20].time.split(' ')[1]}</p>
+           
+           
+           <p class="hour1">${data.forecast.forecastday[0].hour[20].temp_c}°C</p>
+            <img class="nubecita" src="https:${data.forecast.forecastday[0].hour[20].condition.icon}" alt="Weather Icon">
+            </h1>
+            
+             <p class="hour2">${data.forecast.forecastday[0].hour[22].time.split(' ')[1]}</p>
+           
+           
+           <p class="hour3">${data.forecast.forecastday[0].hour[22].temp_c}°C</p>
+            <img class="nubecita1" src="https:${data.forecast.forecastday[0].hour[22].condition.icon}" alt="Weather Icon">
+            </h1>
+            
+             <p class="hour4">${data.forecast.forecastday[0].hour[14].time.split(' ')[1]}</p>
+           
+           
+           <p class="hour5">${data.forecast.forecastday[0].hour[14].temp_c}°C</p>
+            <img class="nubecita2" src="https:${data.forecast.forecastday[0].hour[14].condition.icon}" alt="Weather Icon">
+            
+            </h1>
+            <p class="hour6">${data.forecast.forecastday[0].hour[16].time.split(' ')[1]}</p>
+           
+           
+           <p class="hour7">${data.forecast.forecastday[0].hour[16].temp_c}°C</p>
+            <img class="nubecita3" src="https:${data.forecast.forecastday[0].hour[16].condition.icon}" alt="Weather Icon">
+            </h1>
+            <p class="hour8">${data.forecast.forecastday[0].hour[18].time.split(' ')[1]}</p>
+           
+           
+           <p class="hour9">${data.forecast.forecastday[0].hour[18].temp_c}°C</p>
+            <img class="nubecita4" src="https:${data.forecast.forecastday[0].hour[18].condition.icon}" alt="Weather Icon">
+            </h1>
+            
 
             `
         }
